cbbc6ae2fb4b181e74fbe81342b4c29e2ab223b5
reinbaarsma <rein@solidwebcode.com>
2014-09-22 14:25:31 +0200

    added some styles and font for h1

--- wp-content/plugins/icelandic-horse/page-icelandic_horse.php
+++ wp-content/plugins/icelandic-horse/page-icelandic_horse.php
@@ -19,46 +19,53 @@ get_header();
 				while ( have_posts() ) : the_post();
 					$custom = get_post_custom();
 					$data = unserialize($custom['data'][0]); 
+					$stamboom = unserialize($custom['stamboom'][0]); 
 					?>
-					<article id="post-<?php the_ID(); ?>" <?php post_class('row'); ?>>
-						<h1 class="entry-title"><?php the_title(); ?></h1>
-						<div class="col-md-4">
-							<?php if (isset($data['gender']) && $data['gender']): ?>
-							<dl class="dl-horizontal">
-							  <dt><?php _e('Geslacht', 'icelandic-horse'); ?></dt>
-							  <dd><?php echo _e(ucfirst($data['gender'])); ?></dd>
-							</dl>
-							<?php endif ;?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<h1 class="entry-title">
+							<?php the_title(); ?>
 							<?php if (isset($data['feif']) && $data['feif']): ?>
-							<dl class="dl-horizontal">
-							  <dt><?php _e('FEIF Nummer', 'icelandic-horse'); ?></dt>
-							  <dd><?php echo $data['feif']; ?></dd>
-							</dl>
+							<small>
+							  <?php echo $data['feif']; ?>
+							</small>
 							<?php endif ;?>
-							<?php if (isset($data['geboortedatum']) && $data['geboortedatum']): ?>
-							<dl class="dl-horizontal">
-							  <dt><?php _e('Geboortedatum', 'icelandic-horse'); ?></dt>
-							  <dd><?php echo $data['geboortedatum']; ?></dd>
-							</dl>
-							<?php endif ;?>
-							<?php if (isset($data['kleur']) && $data['kleur']): ?>
-							<dl class="dl-horizontal">
-							  <dt><?php _e('Kleur', 'icelandic-horse'); ?></dt>
-							  <dd><?php echo $data['kleur']; ?></dd>
-							</dl>
-							<?php endif ;?>
+						</h1>
+						<div class="row">
+							<div class="col-md-6">
+								<?php if (isset($data['geboortedatum']) && $data['geboortedatum']): ?>
+								<dl class="dl-horizontal">
+								  <dt><?php _e('Geboortedatum', 'icelandic-horse'); ?></dt>
+								  <dd><?php echo $data['geboortedatum']; ?></dd>
+								</dl>
+								<?php endif ;?>
+								<?php if (isset($data['kleur']) && $data['kleur']): ?>
+								<dl class="dl-horizontal">
+								  <dt><?php _e('Kleur', 'icelandic-horse'); ?></dt>
+								  <dd><?php echo $data['kleur']; ?></dd>
+								</dl>
+								<?php endif ;?>
+								
+								<?php if (isset($stamboom['name']['m']) && $stamboom['name']['m']): ?>
+								<dl class="dl-horizontal">
+								  <dt><?php _e('Moeder', 'icelandic-horse'); ?></dt>
+								  <dd><?php echo $stamboom['name']['m']; ?> <small><?php echo $stamboom['feif']['m']; ?></small></dd>
+								</dl>
+								<?php endif ;?>
+								<?php if (isset($stamboom['name']['m']) && $stamboom['name']['m']): ?>
+								<dl class="dl-horizontal">
+								  <dt><?php _e('Vader', 'icelandic-horse'); ?></dt>
+								  <dd><?php echo $stamboom['name']['f']; ?> <small><?php echo $stamboom['feif']['f']; ?></small></dd>
+								</dl>
+								<?php endif ;?>
+							</div>
+							<div class="col-md-6">
+								<?php the_post_thumbnail( array(300,200), array('class'=>'img-responsive img-rounded') ); ?> 
+								<br>
+								<a href="<?php the_permalink(); ?>" class="btn btn-info"><?php echo _e("Bekijken"); ?></a>
+							</div>
 						</div>
-						<div class="col-md-4">
-							<?php the_post_thumbnail( array(300,200), array('class'=>'img-responsive img-rounded') ); ?> 
-						</div>
-
-						<div class="col-md-4">
-							<div class="entry-content description clearfix">
-								<?php the_excerpt(); ?>
-							</div><!-- .entry-content -->
-
-							<?php get_template_part( 'content', 'footer' ); ?>
-						</div>
+						
+						
 					</article><!-- #post-<?php the_ID(); ?> -->
 
 					<?php
